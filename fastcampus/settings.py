f9fee92fea792db5835edb3e3a59ac9f9531eb49
jang <61114293+jang777@users.noreply.github.com>
2022-05-25 21:19:27 +0900

    Update settings.py

--- fastcampus/settings.py
+++ fastcampus/settings.py
@@ -79,8 +79,14 @@ WSGI_APPLICATION = 'fastcampus.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'project1',
+        'USER': 'admin',
+        'PASSWORD': 'dkagh.123',
+        'HOST': 'database-2.chlswcsrsbnr.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '3306',
+        'OPTIONS' :{
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
     }
 }
 
